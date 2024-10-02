5bdabe44688ccf89243f6750dbc1a550a34d9f46
wanmuhtd <ryskrnzo@gmail.com>
2024-10-03 05:28:20 +0700

    Adding Glide Library dependencies and some adjustment

--- app/src/main/java/train/android/myrecyclerview/Hero.kt
+++ app/src/main/java/train/android/myrecyclerview/Hero.kt
@@ -5,5 +5,5 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Hero(
-    val name: String, val description: String, val photo: Int
+    val name: String, val description: String, val photo: String
 ) : Parcelable
